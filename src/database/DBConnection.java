3dbc35bf23ddec6b9701a163538483020124ce12
Tushar-devs <tushardevs@outlook.com>
2026-09-09 17:34:57 +0530

    Move DB Credentials to config.properties

--- src/database/DBConnection.java
+++ src/database/DBConnection.java
@@ -3,25 +3,36 @@ package database;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
 
 public class DBConnection {
 
-    private static final String URL = "jdbc:mysql://localhost:3306/library_management";
-    private static final String USERNAME = "root";
-    private static final String PASSWORD = "ts##25012003";
+    private static String URL;
+    private static String USERNAME;
+    private static String PASSWORD;
+
+    static {
+        try {
+            Properties props = new Properties();
+            props.load(new FileInputStream("config.properties"));
+            URL = props.getProperty("db.url");
+            USERNAME = props.getProperty("db.username");
+            PASSWORD = props.getProperty("db.password");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
     public static Connection getConnection() {
-
         try {
             Connection connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
             return connection;
-
         } catch (SQLException e) {
             System.out.println("Database Connection Failed!");
             e.printStackTrace();
             return null;
         }
-
     }
-
 }
